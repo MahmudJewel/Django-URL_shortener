15fa19b0993c1f98878e23c822d3f855459a5fab
villain <jmbengal.mbstu@gmail.com>
2022-09-06 04:05:09 +0600

    custom url and expiration for auth user done

--- urlShortener/forms.py
+++ urlShortener/forms.py
@@ -7,8 +7,12 @@ class ShortenerForm(forms.ModelForm):
 
     long_url = forms.URLField(widget=forms.URLInput(
         attrs={"class": "form-control form-control-lg", "placeholder": "Your URL to shorten"}))
+    short_url = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={'size': '15', "class": "form-control form-control-lg", "placeholder": "Enter Custom URL (Optional)"}))
+    expiration_day = forms.IntegerField(required=False, initial=1, widget=forms.NumberInput(
+        attrs={"class": "form-control form-control-lg", "placeholder": "Enter expiration day (Optional)"}))
 
     class Meta:
         model = ShortURLS
 
-        fields = ('long_url',)
+        fields = ('long_url','short_url','expiration_day')
