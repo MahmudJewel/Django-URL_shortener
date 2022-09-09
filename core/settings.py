3180a69c167290b9ebb8e79e822a644a80450ae4
villain <jmbengal.mbstu@gmail.com>
2022-09-09 16:40:10 +0600

    static files for deployment

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -152,6 +152,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangoAutoStatic') #New! #collectstatic==> automatically created 'djangoAutoStatic' folder.
 
 STATIC_URL = 'static/'
 
